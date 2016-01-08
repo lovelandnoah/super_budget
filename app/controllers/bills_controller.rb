2e91dc817d012ba858f7d93165220fe32bc8e47c
Natasha Mays <tash.mays@gmail.com>
2016-01-08 13:15:44 -0700

    second time

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,2 +1,12 @@
 class BillsController < ApplicationController
+	def create
+		@bill = bills.create(bill_params)
+		render 'bill'
+	end
+
+	private
+
+	def bill_params
+		params.require(:bill).permit(:name, :amount)
+	end
 end
